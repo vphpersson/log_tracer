94c39fd7a9a9d6a72f96d8484fd8e267d4921f46
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2023-08-06 23:14:43 +0200

    Refactor.

--- log_tracer/structures/process_info.py
+++ log_tracer/structures/process_info.py
@@ -1,6 +1,5 @@
-from __future__ import annotations
 from dataclasses import dataclass
-from typing import Optional
+from typing import Self
 from datetime import datetime
 from shlex import join as shlex_join
 
@@ -8,21 +7,19 @@ from shlex import join as shlex_join
 @dataclass
 class ProcessInfo:
     name: str
-    # pgid: ...
     pid: int
-    ppid: int
-    # entity_id: ...
     user_name: str
     user_id: int
-    args: Optional[list[str]] = None
-    executable: Optional[str] = None
-    exec_time: Optional[datetime] = None
-    end: Optional[datetime] = None
-    exit_code: Optional[int] = None
-    start: Optional[datetime] = None
-    uptime_ns: Optional[int] = None
-    working_directory: Optional[str] = None
-    parent: Optional[ProcessInfo] = None
+    args: list[str] | None = None
+    executable: str | None = None
+    exec_time: datetime | None = None
+    end: datetime | None = None
+    exit_code: int | None = None
+    ppid: int | None = None
+    start: datetime | None = None
+    uptime_ns: int | None = None
+    working_directory: str | None = None
+    parent: Self | None = None
 
     @property
     def arg_count(self) -> int:
